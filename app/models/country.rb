67fddf37280ead9cd7d6d31b66e963c8e3eca82b
Armen Vartan <vartan.armen@gmail.com>
2014-08-08 18:26:08 -0400

    added model and migration info

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,3 @@
 class Country < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :dictator
 end
--- app/models/dictator.rb
+++ app/models/dictator.rb
@@ -1,3 +1,3 @@
 class Dictator < ActiveRecord::Base
-  # Remember to create a migration!
+  has_many :countries
 end
